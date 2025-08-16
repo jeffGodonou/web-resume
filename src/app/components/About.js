21ea1205f48747bd41d2caa867ce65f57c4e5b50
jeffGodonou <jeff1949@hotmail.fr>
2025-08-16 04:47:49 -0400

    About me page adjustment

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -1,18 +1,18 @@
 export default function About() {
     return (
         <section className="p-6 max-w-3xl mx-auto">
-            <h2 className="text-2xl font-semibold mb-2">About Me</h2>
-            <h3 className="text-xl font-semibold italic mb-2">How can I help you?</h3>
+            <h1 className="text-2xl font-semibold mb-2">About Me</h1>
+            <h2 className="text-xl font-semibold italic mb-2">How can I help you?</h2>
             <p>
                Detail-oriented Computer Science Engineering graduate with hands-on experience in software development using Java, C++, Python, Javascript, Typescript and HTML/SCSS. I am proficient in
 full stack and web applications with a strong knowledge in
 testing, REST APIs, and low-level programming. Known for
 curiosity, autonomy, and a commitment to excellence in
-engineering practices, I am always on the go to learn something new about tech and programming. 
-            
+engineering practices, I am always on the go to learn something new about tech and programming. <br/>
+   <br/>
 I am also passionate about the world and often looking for ways to serve and improve my environment. 
-My main way to go is how can I help?
-
+My main way to go is how can I help? <br/>
+    <br/>
 I am fluent in both French and English with a side project of learning Italian and Spanish.
             </p>
         </section>
